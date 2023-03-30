e0721607347e4dcdf54417cbeba8abc71d23b351
narenMagarZ <narenmagarz98@gmail.com>
2023-03-30 21:26:10 +0545

    ✨ feat(about): add AboutDev component to display information about the DEV community and Forem software

---
+++ app/src/components/about/index.tsx
@@ -0,0 +1,27 @@
+
+
+const AboutDev = () => {
+  return (
+    <div className="container d-flex flex-column justify-content-center align-items-center vh-100">
+     <div
+     className='bg-white border rounded p-5'
+     >
+          <h1 className="mb-3">About DEV</h1>
+          <div className="fs-17">
+          <p>
+               DEV is a community of software developers getting together to help one another out. The software industry relies on collaboration and networked learning. We provide a place for that to happen.
+          </p>
+          <p>
+               DEV is built on <a href="https://forem.com"> Forem: </a>open source software designed to empower communities. Because our application is <a href="https://github.com/forem">open source,</a> you can inspect every little detail of the code, or chip in yourself! Forem is available for anyone interested in creating similar communities in any niche or passion. Visit our meta Forem, <a href="https://forem.dev">forem.dev</a> for more information.
+          </p>
+          <p>
+               We believe in transparency and adding value to the ecosystem. We hope you enjoy poking around and participating!
+          </p>
+          </div>
+
+     </div>
+    </div>
+  );
+};
+
+export default AboutDev
