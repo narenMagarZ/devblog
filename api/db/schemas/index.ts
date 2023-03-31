b022065ab1ede3d8d4f1da1ac423424d18ec2bc4
narenMagarZ <narenmagarz98@gmail.com>
2023-03-31 21:08:59 +0545

    exporting all the models

--- api/db/schemas/index.ts
+++ api/db/schemas/index.ts
@@ -1,8 +1,11 @@
 import articleModel from "./article-schema"
 import userModel from "./userschema" 
-import reactionOnPostModel from "./reaction-on-post-schema"
+import reactionOnPostModel from "./reaction"
 import followersModel from "./followers"
 import commentOnPostModel from "./comment-schema"
+import tagModel from "./tag-schema"
+import notificationModel from "./notification"
+import reactionModel from "./reaction"
 
 
 export const Article = articleModel
@@ -10,3 +13,8 @@ export const User = userModel
 export const ReactionOnPost = reactionOnPostModel
 export const Follower = followersModel
 export const Comment = commentOnPostModel
+export const Tag = tagModel
+export {
+     notificationModel as Notification,
+     reactionModel as Reaction
+}
