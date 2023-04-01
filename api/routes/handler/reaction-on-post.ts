2cbfc43cdda3ba17fb1451f2c4d2543172412380
narenMagarZ <narenmagarz98@gmail.com>
2023-04-01 22:23:07 +0545

    🔨 refactor(reaction-on-post.ts): improve readability and add error handling This commit refactors the `reactionOnPost` function to improve readability and add error handling. The function now uses destructuring to extract the `type` and `articleId` from the request query. It also uses `lodash` to simplify the code and improve readability. Error handling has been added to check if the `articleId` and `type` are present in the request query. If not, a 400 error is returned. If the article is not found, a 404 error is returned.

--- api/routes/handler/reaction-on-post.ts
+++ api/routes/handler/reaction-on-post.ts
@@ -1,27 +1,137 @@
+import { ObjectId } from 'mongoose';
 import { Request, Response } from 'express'
-import { Article } from '../../db/schemas'
+import { Article, Reaction, User } from '../../db/schemas'
+import objectId from '../../utils/str-to-objectid'
+import _ from 'lodash'
 
 async function reactionOnPost(
      req:Request<{},{},{},{
-          type:string,
-          articleId:string,
-          articleSlug:string
+          type:string
+          articleId:string
      }>,
      res:Response
 ){
-     const type = req.query.type || 'like'
-     const me = req.user as User
-     let userName = me['userName']
-     if(!userName){
+     const {
+          type,
+          articleId
+     } = req.query
+     const user = req.user as User
+     if(!user){
           return res.status(401).json({
                error:'Authentication required'
           })
      }
+     if(!articleId || !type)
+          return res.status(400).json({
+               error:'missing articleId or type or task'
+          })
      try{
-          await Article.
+          const article = await Article.findOne({articleId})
+          if(article){
+               const userId = user.id
+               const reaction  = await Reaction.aggregate([
+                    {
+                    $match:{
+                         articleRId:article._id,
+                    }
+               },
+               {
+                    $project:{
+                         _id:0,
+                         reaction:{
+                              $filter:{
+                                   input:'$reactors',
+                                   as:'reactor',
+                                   cond:{
+                                        $eq:['$$reactor.userId',objectId(userId)]
+                                   }
+                              }
+                         }
+                    }
+               }
+          ])
+          const userReaction = {
+               like:false,
+               unicorn:false,
+               raisedHands:false,
+               explodingHead:false,
+               fire:false
+          }
+          if(_.isEmpty(reaction)){
+               await Promise.all([
+                    article.updateOne({
+                         $inc:{
+                              likes:1,
+                              [`reactions.${type}`]:1
+                         }
+                    }),
+                    new Reaction({
+                         articleRId:article._id,
+                         reactors:[
+                              {
+                                   userId:objectId(userId),
+                                   ...userReaction,
+                                   [type]:true
+                              }
+                         ]
 
+                    }).save()
+               ])
+          }
+          else if(_.isEmpty(reaction[0].reaction)){
+               await Promise.all([
+                    article.updateOne({
+                         $inc:{
+                              likes:1,
+                              [`reactions.${type}`]:1
+                         }
+                    }),
+                    Reaction.findOneAndUpdate({
+                         articleRId:article._id
+                    },{
+                         $push:{
+                              reactors:{
+                                   userId:objectId(userId),
+                                   ...userReaction,
+                                   [type]:true
+                              }
+                         }
+                    })
+               ])
+          }
+          else {
+               const reaction = await Reaction.findOne({
+                    articleRId:article._id
+               }) 
+               if(reaction){
+                    let incValue = 1
+                    const index = reaction.reactors.findIndex(reactor=>{
+                         const id = reactor.userId as ObjectId
+                         return id.toString() === userId 
+                    })
+                    if(_.gte(index,0)){
+                         const typeValue = reaction.reactors[index][type]
+                         if(typeValue) incValue = -1
+                         reaction.reactors[index][type] = !typeValue
+                         await reaction.save()
+                         await article.updateOne({
+                              $inc:{
+                                   likes:incValue,
+                                   [`reactions.${type}`]:incValue
+                              }
+                         })
+                    }
 
+               }
 
+          }
+          return res.status(200).json({
+               message:'ok'
+          })
+          }
+          else return res.status(404).json({
+               message:'article not found'
+          })
      }
      catch(err){
           console.error(err)
