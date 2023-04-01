6fd0bf9a320c0a00a59d3a6ec3365337233260ca
narenMagarZ <narenmagarz98@gmail.com>
2023-04-01 22:25:28 +0545

    🎉 feat(app.ts): add passport-jwt authentication and cors middleware This commit adds passport-jwt authentication middleware to the application. It also adds cors middleware to allow cross-origin requests from the frontend. The passport-jwt middleware is used to authenticate requests using JSON Web Tokens (JWTs). The cors middleware is configured to allow requests from http://localhost:3000.

--- api/app.ts
+++ api/app.ts
@@ -1,14 +1,75 @@
-import express from 'express'
-import { router } from './routes'
+import express, { 
+     urlencoded,
+     Request,
+     json
+     } from 'express'
+import { apiRouter } from './routes'
+import cors from 'cors'
+import path from 'path'
+import passport from 'passport'
+import {Strategy as JwtStrategy} from 'passport-jwt'
+import { User } from './db/schemas'
+import cookieParser from 'cookie-parser'
 
+export const app = express()
 
-export default function app(){
-     try{
-          const server = express()
-          server.use('/',router)
-          return server
-     }
-     catch(err){
-          console.error(err)
-     }
+try{
+
+     app.use(passport.initialize())
+     app.use(cookieParser(process.env.SECRET_KEY as string))
+     passport.use(new JwtStrategy({
+          secretOrKey:process.env.SECRET_KEY as string,
+          jwtFromRequest:(req:Request)=>{
+               let token = null
+               if(req) token = req.signedCookies.token
+               return token
+          }
+     },async function (payload,done) {
+          const {
+               id,
+          } = payload
+          try{
+               const user = await User.findById(id)
+               if(user){
+                    const {
+                         userName,
+                         name,
+                         email,
+                         id:uId
+                    } = user
+                    return done(null,{
+                         userName,
+                         name,
+                         email,
+                         id:uId
+                    })
+
+               }
+               else return done(null,{
+                    userName:null
+               })
+          }
+          catch(err){
+               return done(err,false)
+          }
+
+     }))
+
+     app.use(cors({
+          origin:'http://localhost:3000',
+          optionsSuccessStatus : 200,
+          credentials : true,
+          methods : "GET,HEAD,PUT,PATCH,POST,DELETE"
+     }))
+     app.use('/image',express.static(path.resolve(__dirname,'images')))
+     app.use(json())
+     app.use(urlencoded({
+          extended:false
+     }))
+
+     app.use('/api',apiRouter)
 }
+catch(err){
+     console.error(err)
+}
+
