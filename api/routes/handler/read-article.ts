45022cdb86049228ee0c69bbb0d353b9089080b4
narenMagarZ <narenmagarz98@gmail.com>
2023-04-01 22:21:25 +0545

    🔨 refactor(read-article.ts): refactor readArticle function ✨ feat(read-article.ts): add support for extracting article id from slug, extracting comment, and converting string to ObjectId The readArticle function has been refactored to improve readability and maintainability. The function now uses destructuring to extract the required parameters from the request object. The function also uses const instead of let to declare variables that are not reassigned. Support for extracting the article id from the slug, extracting comments, and converting a string to ObjectId has been added. These changes improve the overall functionality of the readArticle function.
    🐛 fix(read-article.ts): change joined to createdAt and remove console.log statements
    ✨ feat(read-article.ts): add support for brandColor and userReaction
    The joined field in the user object is now renamed to createdAt to match the naming convention. Console.log statements are removed. Support for brandColor and userReaction is added to the article object. The commentsOnPost field is extracted and returned as a separate comments array.

--- api/routes/handler/read-article.ts
+++ api/routes/handler/read-article.ts
@@ -4,15 +4,13 @@ import {
      Request,
      Response
 } from 'express'
+import _ from 'lodash'
 import { Article } from '../../db/schemas'
+import extractArticleIdFromArticleSlug from '../../utils/extract-article-id-from-slug'
+import extractComment from '../../utils/extract-comment'
+import objectId from '../../utils/str-to-objectid'
+import dayjs from 'dayjs'
 
-
-interface User {
-     uId:string
-     name:string
-     userName:string
-     email:string
-}
 async function readArticle(
      req:Request<{
           user:string,
@@ -21,39 +19,39 @@ async function readArticle(
      res:Response
 ){
 
-     let me = req.user as User
-     let userName = me['userName']
-     if(!userName){
-          return res.status(401).json({
-               error:'Authentication required'
+     const user = req.user as User
+     const userId = user.id
+     const {
+          user:userName,
+          articleslug:slug
+     } = req.params
+     if(!userName || !slug){
+          return res.status(400).json({
+               message:'missing username or slug'
           })
      }
-     const {
-          user:owner,
-          articleslug:articleSlug
-     } = req.params
-
-     const index = articleSlug.lastIndexOf('-')
-     const articleId = articleSlug.slice(index+1,articleSlug.length)
-     const url = '/'.concat(
-          owner,
-          '/',
-          articleSlug)
+     const articleId = extractArticleIdFromArticleSlug(slug)
      try{
+          const test = await Article.aggregate([
+               {
+                    $match:{
+                         articleId
+                    }
+               }
+          ])
+          console.log(test,'test')
           const thisArticle = await Article.aggregate([
                {
                     $match:{
-                         articleId,
-                         url,
-                         owner
+                         articleId
                     }
                },
                {
                     $lookup:{
                          from:'users',
                          as:'userInfo',
-                         localField:'owner',
-                         foreignField:'userName',
+                         localField:'userId',
+                         foreignField:'_id',
                          pipeline:[
                               {
                                    $project:{
@@ -67,61 +65,148 @@ async function readArticle(
                                         picture:1,
                                         work:1,
                                         education:1,
-                                        joined:1,
+                                        createdAt:1,
                                         bio:1,
-                                        location:1
+                                        location:1,
+                                        brandColor:1
                                    }
                               }
                          ]
                     }
                },
+               {
+                    $lookup:{
+                         from:'comments',
+                         as:'commentsOnPost',
+                         localField:'_id',
+                         foreignField:'postId',
+                         pipeline:[
+                              {
+                                   $lookup:{
+                                        from:'users',
+                                        as:'user',
+                                        localField:'commentBy',
+                                        foreignField:'userName',
+                                        pipeline:[
+                                             {
+                                                  $project:{
+                                                       _id:0,
+                                                       userName:1,
+                                                       name:1,
+                                                       url:1,
+                                                       picture:1,
+                                                       bio:1,
+                                                       work:1,
+                                                       education:1,
+                                                       location:1,
+                                                       createdAt:1,
+                                                       theme:1
+                                                  }
+                                             }
+                                        ]
+                                   }
+                              },
+                              {
+                                   $project:{
+                                        _id:0,
+                                        id:{
+                                             $toString:'$_id'
+                                        },
+                                        parentCommentId:{
+                                             $toString:'$parentCommentId'
+                                        },
+                                        text:1,
+                                        commentBy:1,
+                                        createdAt:1,
+                                        likes:1,
+                                        isLikedByYou:{
+                                             $in:[objectId(userId),'$reactors']
+                                        },
+                                        user:1
+                                   }
+                              }
+                         ]
+                    }
+               },
+               {
+                    $lookup:{
+                         from:'reactions',
+                         as:'userReaction',
+                         localField:'_id',
+                         foreignField:'articleRId',
+                         pipeline:[{
+                              $project:{
+                                   _id:0,
+                                   reactor:{
+                                        $filter:{
+                                             input:'$reactors',
+                                             as:'reactor',
+                                             cond:{
+                                                  $eq:['$$reactor.userId',objectId(userId)]
+                                             }
+                                        }
+                                   }
+                              }
+                         }]
+                    }
+               },
                   {
                     $project:{
                          _id:0,
+                         articleId:1,
                          title:1,
                          likes:1,
                          postBy:1,
                          reactions:1,
+                         numUsersAddedToReadingList:1,
                          comments:1,
                          content:1,
-                         isBookMarked:1,
+                         isBookMarked:{
+                              $in:[objectId(userId),'$readingListUserIds']
+                         },
                          coverImageUrl:1,
                          tags:1,
-                         userInfo:1,
-                         yourReaction:1,
+                         userInfo:{
+                              $arrayElemAt:['$userInfo',0]
+                         },
+                         commentsOnPost:1,
+                         userReaction:{
+                              $arrayElemAt:[{
+                                   $arrayElemAt:['$userReaction.reactor',0]
+                              },0]
+                         }
                     }
                }
           ])
-          if(thisArticle[0]){
+          console.log(thisArticle,'thisArticle')
+          if(!_.isEmpty(thisArticle)){
                const {
                     userInfo,
                     ...articleInfo
                } = thisArticle[0]
-               console.log(userInfo[0])
-               console.log(articleInfo)
-               if(!articleInfo['yourReaction'] && !articleInfo['reactions']){
-                    articleInfo['yourReaction'] = {
-                         like:false,
-                         unicorn:false,
-                         explodingHead:false,
-                         raisedHands:false,
-                         fire:false
-                    } 
-                    articleInfo['reactions'] = {
-                         like:0,
-                         unicorn:0,
-                         explodingHead:0,
-                         raisedHands:0,
-                         fire:0
-                    }
+               const articleComments = extractComment(articleInfo.commentsOnPost)
+               delete articleInfo.commentsOnPost
+               const isMe = user.userName === userName
+               let temp = articleInfo.userReaction ||={
+                    like:false,
+                    fire:false,
+                    raisedHands:false,
+                    explodingHead:false,
+                    unicorn:false
                }
-               const isMe = userName === owner 
+               if(!_.isEmpty(temp)){
+                    articleInfo.userReaction = _.omit(temp,['_id','userId'])
+               }
+               const joinedAt = dayjs(userInfo.createdAt).format('MMM DD, YYYY')
+               delete userInfo.createdAt
                return res.status(200).json({
                     user:{
-                         ...userInfo[0],
+                         ...userInfo,
+                         joined:joinedAt,
                          isMe
                     },
-                    article:articleInfo
+                    article:articleInfo,
+                    comments:articleComments
                })
 
           }
