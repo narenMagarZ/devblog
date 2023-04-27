8048fad1eedfafafc5a0e79a7674b9a4bbf8015d
narenMagarZ <narenmagarz98@gmail.com>
2023-04-27 21:07:17 +0545

    base file to export all the handlers for router

---
+++ api/routes/handler/index.ts
@@ -0,0 +1,37 @@
+
+import fetchArticlesPreview from "./fetch-articles-preview";
+import uploadImage from "./upload-image";
+import upload from "../../utils/file_handler";
+import updateProfile from "./update-profile";
+import githubEnter from "./auth/github-enter";
+import twitterEnter from "./auth/twitter-enter";
+import customizeAccount from "./customize-account";
+import searchBlogPost from "./search-blogpost";
+import generateImageUrl from "./generate-image-url";
+import authCheck from "./auth-check";
+import manageArticle from "./manage-article";
+import editArticle from "./edit-article";
+import confirmDelete from "./confirm-delete";
+import deleteArticle from "./delete-article";
+import createArticleDraft from "./create-article-draft";
+import createArticlePreview from "./create-article-preview";
+import addArticleToReadList from "./add-article-to-list";
+export {
+     fetchArticlesPreview,
+     upload,
+     uploadImage,
+     updateProfile,
+     githubEnter,
+     twitterEnter,
+     customizeAccount,
+     authCheck,
+     manageArticle,
+     searchBlogPost,
+     generateImageUrl,
+     editArticle,
+     confirmDelete,
+     deleteArticle,
+     createArticleDraft,
+     createArticlePreview,
+     addArticleToReadList
+}
