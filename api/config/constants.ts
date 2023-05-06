af40b85fda39bb065fca0dee20ad442520bd9d5a
narenMagarZ <narenmagarz98@gmail.com>
2023-05-06 08:54:49 +0545

    added the api response message

---
+++ api/config/constants.ts
@@ -0,0 +1,22 @@
+const MISSING_PARAMETERS = "Missing required parameters";
+const AUTH_FAILED = "Authentication failed";
+const USER_CREATED = "User created successfully";
+const BAD_REQUEST = "Bad request";
+const SERVER_ERROR = "Internal server error";
+const USER_LOGGED_IN = 'Logged in successfully'
+const USER_PROFILE_UPDATED = 'User\'s profile updated successfully'
+const USER_NOT_FOUND = 'User not found'
+const ARTICLE_NOT_FOUND = 'Article not found'
+const MISSING_USERNAME = 'Missing username'
+export {
+     MISSING_PARAMETERS,
+     AUTH_FAILED,
+     USER_CREATED,
+     BAD_REQUEST,
+     SERVER_ERROR,
+     USER_LOGGED_IN,
+     USER_PROFILE_UPDATED,
+     USER_NOT_FOUND,
+     ARTICLE_NOT_FOUND,
+     MISSING_USERNAME
+}
